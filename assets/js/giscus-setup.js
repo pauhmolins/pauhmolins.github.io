98fcf92b679a69748ac2c4ad97b679e278fb8e57
pauhmolins <90088771+pauhmolins@users.noreply.github.com>
2025-09-12 17:59:40 +0000

    Deploying to gh-pages from @ pauhmolins/pauhmolins.github.io@3c7f5f9a7bc7aa867de3618c79f71a0267bd1cc9 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "pauhmolins/pauhmolins",
+    "data-repo": "pauhmolins/pauhmolins.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
